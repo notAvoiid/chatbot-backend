a216d89f8888f7f2521516e5c6e187d2d7999054
Igor <igor.nacimento51@gmail.com>
2024-03-07 13:52:28 -0300

    "initial logic to produce answers"

--- src/main/java/com/museum/chatbot/controllers/FaqController.java
+++ src/main/java/com/museum/chatbot/controllers/FaqController.java
@@ -1,7 +1,8 @@
 package com.museum.chatbot.controllers;
 
 import com.museum.chatbot.dto.MessageRequest;
-import com.museum.chatbot.utils.FaqAnswers;
+import com.museum.chatbot.dto.MessageResponse;
+import com.museum.chatbot.services.FaqService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,11 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/chat")
 public class FaqController {
 
+    private final FaqService service;
+
+    public FaqController(FaqService service) {
+        this.service = service;
+    }
+
     @PostMapping
-    public ResponseEntity<String> answerQuestion(@RequestBody MessageRequest message){
-        FaqAnswers faqAnswers = new FaqAnswers();
-        System.out.println(faqAnswers.getAnswers().get(0).getAnswer());
-        return ResponseEntity.ok().body("Deu certo!");
+    public ResponseEntity<MessageResponse> answerQuestion(@RequestBody MessageRequest message){
+        String answer = service.getAnswers(message.message());
+        MessageResponse response = new MessageResponse(answer);
+        return ResponseEntity.ok().body(response);
     }
 
 }
--- src/main/java/com/museum/chatbot/services/FaqService.java
+++ src/main/java/com/museum/chatbot/services/FaqService.java
@@ -1,7 +1,28 @@
 package com.museum.chatbot.services;
 
+import com.museum.chatbot.domain.FaqAnswer;
+import com.museum.chatbot.utils.FaqAnswers;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class FaqService {
+
+    private final FaqAnswers faqAnswers = new FaqAnswers();
+
+    public String getAnswers(String message){
+        String[] words = message.toLowerCase().split("\\s+");
+        List<String> wordsList = Arrays.asList(words);
+        for (FaqAnswer entry : faqAnswers.getAnswers()) {
+            for (String keyword : entry.getKeywords()) {
+                if (wordsList.contains(keyword)) {
+                    return entry.getAnswer();
+                }
+            }
+        }
+
+        return faqAnswers.getDefaultAnswer();
+    }
 }
